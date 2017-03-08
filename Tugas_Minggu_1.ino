447f5721fcf15e95ac143e44ba8ec4e1f4d63620
Galih Dwi Susanto <galihdwi53.gd@gmail.com>
2017-03-08 22:10:40 +0700

    Add files via upload

---
+++ Tugas_Minggu_1.ino
@@ -0,0 +1,10 @@
+void setup() //fungsi untuk inisialisasi program yang dibuat
+{
+Serial.begin(9600);  //Setting kecepatan data dalam bits per second (baud) untuk penyampaian data serial
+}
+
+void loop() //Fungsi yang programnya untuk dieksekusi berulang-ulang
+{
+  Serial.print("Tugas Operasi Mikroprosesor Minggu ke-1\n"); //Menampilkan string di serial monitor
+delay(2000); //Memberi jeda waktu selama 2 detik
+}
